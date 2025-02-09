08b9e953c758cd191729bc6ba3caac855ec20862
OusamaAsherM <ashermooche03@gmail.com>
2025-02-09 13:15:43 +0200

    Update page.tsx

--- app/anime/[id]/page.tsx
+++ app/anime/[id]/page.tsx
@@ -66,6 +66,7 @@ async function getAnime(id: string): Promise<Anime | undefined> {
 }
 
 export default async function Page({ params }: PageProps) {
+  // Async fetch of anime data based on params.id
   const anime = await getAnime(params.id)
 
   if (!anime) {
